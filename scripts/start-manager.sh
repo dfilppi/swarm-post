f4740435dc908dead5bc12678b95a4b172c09009
DeWayne Filppi <dewayne@gigaspaces.com>
2016-09-16 15:13:29 -0700

    reorg

--- scripts/start-manager.sh
+++ scripts/start-manager.sh
@@ -2,5 +2,9 @@
 
 sudo docker swarm init --advertise-addr $IP
 
-ctx instance runtime-properties master_token $(sudo docker swarm join-token -q manager)
+ctx instance runtime-properties manager_token $(sudo docker swarm join-token -q manager)
 ctx instance runtime-properties worker_token $(sudo docker swarm join-token -q worker)
+
+# If there are existing nodes, tell them to rejoin (heal scenario)
+
+
